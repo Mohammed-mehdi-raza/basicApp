5d09588cb52c8c94b5f44633ce3b1dabeda66e40
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2024-08-11 11:56:48 +0530

    initialized project

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors());
+app.get('/test',(req,res)=>{
+    // res.sendStatus(200);
+    // res.setHeader("content-type","text/html");
+    res.send("Hello!");
+})
+
+server.listen(PORT,()=>{
+    console.log(`server running on port:${PORT}`);
+})
